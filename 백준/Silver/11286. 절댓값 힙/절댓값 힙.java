851553792ca5f7c39d1c5577ee69fd589bf2c6da
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-03-05 16:09:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 940 ms, Memory: 45528 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int lastindex;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        lastindex = 0;
+        for (int n = 0; n < N; n++) {
+            int cnd = Integer.parseInt(br.readLine());
+            if (cnd != 0) {
+                arr[++lastindex] = cnd;
+                int now = lastindex;
+                while (now > 1 && (Math.abs(arr[now]) < Math.abs(arr[now / 2]) || (Math.abs(arr[now]) == Math.abs(arr[now / 2]) && arr[now] < arr[now / 2]))) {
+                    swap(now, now / 2);
+                    now = now / 2;
+                }
+            } else {
+                if (arr[1] != 0) {
+                    System.out.println(arr[1]);
+                    arr[1] = arr[lastindex];
+                    arr[lastindex--] = 0;
+                    int now = 1;
+                    while (true) {
+                        int child = now * 2;
+                        if (child + 1 <= lastindex && (Math.abs(arr[child]) > Math.abs(arr[child + 1])||(Math.abs(arr[child]) == Math.abs(arr[child + 1])&&arr[child]>arr[child+1]))) {
+                            child++;
+                        }
+                        if (child > lastindex || Math.abs(arr[child]) > Math.abs(arr[now])||(Math.abs(arr[child]) == Math.abs(arr[now])&&arr[child]>arr[now])) {
+                            break;
+                        }
+                        swap(now, child);
+                        now = child;
+                    }
+                } else {
+                    System.out.println(0);
+                }
+            }
+        }
+    }
+
+    static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
