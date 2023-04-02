0778d871ff1dcaf68e98d872f5d6966faf8f3923
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-04-02 11:58:14 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 432 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < 2*N-1; i++) {
+            for (int j = 0; j < Math.min(i,2*(N-1)-i); j++) System.out.print(" ");
+            for(int j = 0; j < Math.max(2*(i-N+1)+1,2*(N-i)-1); j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
