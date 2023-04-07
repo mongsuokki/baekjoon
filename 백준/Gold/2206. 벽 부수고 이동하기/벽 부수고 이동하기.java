5fdf225b6788ba650a45e953532a829c3012d7fe
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-04-07 23:37:27 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 684 ms, Memory: 146508 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,50 +1,64 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] R = {-1, 0, 1, 0}, C = {0, 1, 0, -1};
-        int N = sc.nextInt(), M = sc.nextInt(), min = 1000000;
-        int[][] map = new int[N][M];
-        boolean[][][] visit = new boolean[2][N][M];
-        Queue<now> que = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            String A = sc.next();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = A.charAt(j) - '0';
-            }
+    static class Point{
+        int row, col, cost, length;
+        public Point(int row, int col, int cost, int length) {
+            this.row = row;
+            this.col = col;
+            this.cost = cost;
+            this.length = length;
         }
-        que.add(new now(0, 0, 3, 0));
-        while (!que.isEmpty()) {
-            now temp = que.poll();
-            int nowx = temp.x, nowy = temp.y, nowscore = temp.score, nowbrk = temp.brk;
-            if (nowx == M - 1 && nowy == N - 1) {
-                if (nowscore < min) min = nowscore;
-                continue;
+    }
+    static final int INF = Integer.MAX_VALUE;
+    static int N;
+    static int M;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static char[][] arr;
+    static boolean[][][] visit;
+    static int min = INF;
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new char[N][M];
+        for(int i=0; i<N; i++) {
+            arr[i] = bf.readLine().toCharArray();
+        }
+        visit = new boolean[N][M][2];
+        bfs(new Point(0,0,1,1));
+        if(min == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(min);
+    }
+    
+    static void bfs(Point p) {
+        Queue<Point> pq = new LinkedList<>();
+        visit[p.row][p.col][1] = true;
+        visit[p.row][p.col][0] = true;
+        pq.offer(p);
+        while(!pq.isEmpty()) {
+            Point now = pq.poll();
+            if(now.row == N-1 && now.col == M-1) {
+                min = Math.min(min, now.length);
+                return;
             }
-            if (visit[nowbrk][nowy][nowx]) continue;
-            else visit[nowbrk][nowy][nowx] = true;
-            for (int i = 0; i < 4; i++) {
-                int nextx = nowx + C[i], nexty = nowy + R[i];
-                if (nextx < M && nexty < N && nextx >= 0 & nexty >= 0) {
-                    if (map[nexty][nextx] == 1 && nowbrk == 0) {
-                        que.add(new now(nextx, nexty, nowscore + 1, 1));
-                    } else if (map[nexty][nextx] == 0) {
-                        que.add(new now(nextx, nexty, nowscore + 1, nowbrk));
-                    }
+            for(int i=0; i<4; i++) {
+                int dRow = now.row + dr[i];
+                int dCol = now.col + dc[i];
+                if(dRow >= N || dCol >= M || dRow <0 || dCol < 0 || visit[dRow][dCol][now.cost]) continue;
+                if(visit[dRow][dCol][1] && arr[dRow][dCol] == '1') continue;
+                else if(arr[dRow][dCol] == '1' && now.cost == 1) {
+                    visit[dRow][dCol][now.cost] = true;
+                    pq.offer(new Point(dRow, dCol, 0, now.length + 1));
+                }
+                else if(arr[dRow][dCol] == '0') {
+                    if(now.cost == 1) visit[dRow][dCol][0] = true;
+                    visit[dRow][dCol][now.cost] = true;
+                    pq.offer(new Point(dRow, dCol, now.cost, now.length + 1));
                 }
             }
-        }
-        if (min == 1000000) System.out.println(-1);
-        else System.out.println(min - 2);
-    }
-    static class now {
-        int x,y,score,brk;
-        public now(int x, int y, int score, int brk) {
-            this.x = x;
-            this.y = y;
-            this.score = score;
-            this.brk = brk;
         }
     }
 }
