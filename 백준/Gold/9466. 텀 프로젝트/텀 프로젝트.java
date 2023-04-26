c86f170df645d95c0c64495301ad5ec2886417b3
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-04-26 22:58:01 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1092 ms, Memory: 302748 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,23 +1,25 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         for(int t=0;t<T;t++) {
-            int N = sc.nextInt(),ans=0,temp=1;
+            int N = Integer.parseInt(br.readLine()),ans=0,temp=1;
             int[] arr = new int[N], parr = new int[N];
-            for(int i=0;i<N;i++) arr[i] = sc.nextInt()-1;
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken())-1;
             for(int i=0;i<N;i++) {
                 if(parr[i]!=0) continue;
-                int nowi =i,sttemp = temp;
-                while(parr[nowi]==0){
-                    parr[nowi] = temp++;
-                    nowi = arr[nowi];
+                int ni =i,temp2 = temp;
+                while(parr[ni]==0){
+                    parr[ni] = temp++;
+                    ni = arr[ni];
                 }
-                if(parr[nowi]>sttemp) ans+=parr[nowi]-sttemp;
-                else if(parr[nowi]==sttemp) continue;
-                else ans+= temp-sttemp;
+                if(parr[ni]>temp2) ans+=parr[ni]-temp2;
+                else if(parr[ni]==temp2) continue;
+                else ans+= temp-temp2;
             }
             System.out.println(ans);
         }
