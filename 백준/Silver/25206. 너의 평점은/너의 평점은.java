793b57139424c7cbc64d5be926a33bd60384b7cb
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-05-18 22:17:36 +0900

    [Silver V] Title: 너의 평점은, Time: 136 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,37 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double ans =0;
-        double sum=0;
-        for(int i=0;i<20;i++){
-            sc.next();
-            double A = sc.nextDouble();
-            sum+=A;
-            double C = 0;
-            switch (sc.next()){
-                case "A+": C =4.5;
-                    break;
-                case "A0": C =4.0;
-                    break;
-                case "B+": C =3.5;
-                    break;
-                case "B0": C =3.0;
-                    break;
-                case "C+": C =2.5;
-                    break;
-                case "C0": C =2.0;
-                    break;
-                case "D+": C =1.5;
-                    break;
-                case "D0": C =1.0;
-                    break;
-                case "P":sum-=A;
-                    break;
-            }
-            ans+=A*C;
-        }
-        System.out.println(ans/sum);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double ans = 0, sum = 0;
+		for (int i = 0; i < 20; i++) {
+			sc.next();
+			double A = sc.nextDouble(), C = 0;
+			sum += A;
+			switch (sc.next()) {
+			case "A+":
+				C = 4.5;
+				break;
+			case "A0":
+				C = 4.0;
+				break;
+			case "B+":
+				C = 3.5;
+				break;
+			case "B0":
+				C = 3.0;
+				break;
+			case "C+":
+				C = 2.5;
+				break;
+			case "C0":
+				C = 2.0;
+				break;
+			case "D+":
+				C = 1.5;
+				break;
+			case "D0":
+				C = 1.0;
+				break;
+			case "P":
+				sum -= A;
+				break;
+			}
+			ans += A * C;
+		}
+		System.out.println(ans / sum);
+	}
 }
