b731412339fc0daff279c77f10e3c7ba66437c5d
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-06-23 17:00:28 +0900

    [Gold V] Title: 용액 합성하기, Time: 244 ms, Memory: 26456 KB -BaekjoonHub

---
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()),start = 0, end = N-1,min = 2000000000,minA=0,minB=0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		while(start != end){
+			int A = arr[start], B = arr[end];
+			if(Math.abs(A+B)<min){
+				minA = A;
+				minB = B;
+				min = Math.abs(A+B);
+			}
+			if(A+B<0) start++;
+			else if (A+B>0) end--;
+			else break;
+		}
+		System.out.println(minA+minB);
+	}
+}
