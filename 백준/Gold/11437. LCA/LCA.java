dfc8ad48c2b8c24141268ddb3a9fa1f3bb5f5b8b
mongsuokki <121627931+mongsuokki@users.noreply.github.com>
2023-07-02 00:04:04 +0900

    [Gold III] Title: LCA, Time: 1464 ms, Memory: 50044 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -1,25 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int[] depths,parents;
 	static ArrayList<Integer>[] tree;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		tree = new ArrayList[N+1];
 		depths = new int[N+1];
 		parents = new int[N+1];
 		for(int i=1;i<=N;i++) tree[i] = new ArrayList<Integer>();
 		for(int i=0;i<N-1;i++){
-			int A = sc.nextInt(),B = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken()),B = Integer.parseInt(st.nextToken());
 			tree[A].add(B);
 			tree[B].add(A);
 		}
 		dfs(1,0,0);
-		int M = sc.nextInt();
+		int M = Integer.parseInt(br.readLine());
 		for(int i=0;i<M;i++){
-			int A = sc.nextInt(),B = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken()),B = Integer.parseInt(st.nextToken());
 			System.out.println(lca(A,B));
 		}
 
